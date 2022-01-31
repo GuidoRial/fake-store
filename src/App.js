c64bc81b5b50d3c4e48f8d212170a7fbd0760bbb
GuidoRial <rialguido@gmail.com>
2022-01-31 09:22:42 -0300

    added a few comments

--- src/App.js
+++ src/App.js
@@ -12,39 +12,22 @@ function App() {
     const { productItems } = data;
     const [cartItems, setCartItems] = useState([]);
 
-    const handleAddProduct = (product) => {
-        let updatedProductList;
-
-        if (getProductFromCart(product)) {
-            updatedProductList = updateCartQuantity('add', product)
-        } else {
-            updatedProductList = [...cartItems, { ...product, quantity: 1 }];
-        }
-
-        setCartItems(updatedProductList)
-    };
-
-    const handleRemoveProduct = (product) => {
-        let updatedProductList;
-
-        if (getProductFromCart(product).quantity > 1) {
-            updatedProductList = updateCartQuantity('subtract', product)
-        } else {
-            updatedProductList = cartItems.filter((item) => item.id !== product.id);
-        }
-        
-        setCartItems(updatedProductList)
-    };
-
+    //returns the product
     function getProductFromCart(product) {
         return cartItems.find((item) => item.id === product.id);
     }
 
+    //Takes an operation and a product
     function updateCartQuantity(operation, product) {
-        const indexOfProduct = cartItems.findIndex(item => item.id === product.id);
-        const products = [...cartItems]
+        //returns the index of the products
+        const indexOfProduct = cartItems.findIndex(
+            (item) => item.id === product.id
+        );
+        // clones the array
+        const products = [...cartItems];
 
-        if (operation == 'add') {
+        //change the amount of items in the cart
+        if (operation == "add") {
             products[indexOfProduct].quantity += 1;
         } else {
             products[indexOfProduct].quantity -= 1;
@@ -53,9 +36,35 @@ function App() {
         return products;
     }
 
-        const emptyCart = () => {
-            setCartItems([]);
-        };
+    const emptyCart = () => {
+        setCartItems([]);
+    };
+
+    const handleAddProduct = (product) => {
+        let updatedProductList;
+
+        if (getProductFromCart(product)) {
+            updatedProductList = updateCartQuantity("add", product);
+        } else {
+            updatedProductList = [...cartItems, { ...product, quantity: 1 }];
+        }
+
+        setCartItems(updatedProductList);
+    };
+
+    const handleRemoveProduct = (product) => {
+        let updatedProductList;
+
+        if (getProductFromCart(product).quantity > 1) {
+            updatedProductList = updateCartQuantity("subtract", product);
+        } else {
+            updatedProductList = cartItems.filter(
+                (item) => item.id !== product.id
+            );
+        }
+
+        setCartItems(updatedProductList);
+    };
 
     return (
         <div className="App">
